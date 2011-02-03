ad765e99b569d3201b7bd2c1d8044f82b8293b55
Raptor 2101 <raptor2101@gmx.de>
2011-02-03 23:11:14 +0100

    ADD: orf completed

--- mediathek/orf.py
+++ mediathek/orf.py
@@ -58,17 +58,62 @@ class ORFMediathek(Mediathek):
           TreeNode("1.3",u"Magazine","",False,
             (
               TreeNode("1.3.0",u"Bewusst gesund - das Magazin","http://tvthek.orf.at/programs/1714463-Bewusst-gesund---das-Magazin",True),
-              TreeNode("1.3.0",u"Bürgeranwalt","http://tvthek.orf.at/programs/1339-Buergeranwalt",True),
-              TreeNode("1.3.0",u"Bürgerforum","http://tvthek.orf.at/programs/1343-Buergerforum",True),
-              TreeNode("1.3.0",u"Konkret","http://tvthek.orf.at/programs/1336-Konkret",True),
-              TreeNode("1.3.0",u"Land und Leute","http://tvthek.orf.at/programs/1369-Land-und-Leute",True),
-              TreeNode("1.3.0",u"Stöckl am Samstag","http://tvthek.orf.at/programs/1651-Stoeckl-am-Samstag",True),
-              TreeNode("1.3.0",u"Thema","http://tvthek.orf.at/programs/1319-Thema",True),
-              TreeNode("1.3.0",u"Vera exklusiv","http://tvthek.orf.at/programs/35440-Vera-exklusiv",True),
-              TreeNode("1.3.0",u"Weltjournal","http://tvthek.orf.at/programs/1328-Weltjournal",True),
-              TreeNode("1.3.0",u"Winterzeit","http://tvthek.orf.at/programs/1003023-Winterzeit",True),
+              TreeNode("1.3.1",u"Bürgeranwalt","http://tvthek.orf.at/programs/1339-Buergeranwalt",True),
+              TreeNode("1.3.2",u"Bürgerforum","http://tvthek.orf.at/programs/1343-Buergerforum",True),
+              TreeNode("1.3.3",u"Konkret","http://tvthek.orf.at/programs/1336-Konkret",True),
+              TreeNode("1.3.4",u"Land und Leute","http://tvthek.orf.at/programs/1369-Land-und-Leute",True),
+              TreeNode("1.3.5",u"Stöckl am Samstag","http://tvthek.orf.at/programs/1651-Stoeckl-am-Samstag",True),
+              TreeNode("1.3.6",u"Thema","http://tvthek.orf.at/programs/1319-Thema",True),
+              TreeNode("1.3.7",u"Vera exklusiv","http://tvthek.orf.at/programs/35440-Vera-exklusiv",True),
+              TreeNode("1.3.8",u"Weltjournal","http://tvthek.orf.at/programs/1328-Weltjournal",True),
+              TreeNode("1.3.9",u"Winterzeit","http://tvthek.orf.at/programs/1003023-Winterzeit",True),
+              TreeNode("1.3.10",u"€co","http://tvthek.orf.at/programs/1346-Eco",True),
             )
           ),
+          TreeNode("1.4",u"Kultur","",False,
+            (
+              TreeNode("1.4.0",u"a.viso","http://tvthek.orf.at/programs/1299-a-viso",True),
+              TreeNode("1.4.0",u"Kulturmontag","http://tvthek.orf.at/programs/1303-Kulturmontag",True),
+            )
+          ),
+          TreeNode("1.5",u"Sport","http://tvthek.orf.at/programs/1379-Sportbild",True,
+            (
+            )
+          ),
+          TreeNode("1.6",u"Religion","",False,
+            (
+              TreeNode("1.6.0",u"Kreuz & Quer","http://tvthek.orf.at/programs/1193-Kreuz---Quer",True),
+              TreeNode("1.6.1",u"Orientierung","http://tvthek.orf.at/programs/1366-Orientierung",True),
+              TreeNode("1.6.2",u"Religionen der Welt","http://tvthek.orf.at/programs/1656-Religionen-der-Welt",True),
+              TreeNode("1.6.3",u"Was ich glaube","http://tvthek.orf.at/programs/1287-Was-ich-glaube",True),
+            )
+          ),
+          TreeNode("1.7",u"Society","",False,
+            (
+              TreeNode("1.7.0",u"Chilli","http://tvthek.orf.at/programs/1504477-Chili",True),
+              TreeNode("1.7.1",u"Seitenblicke","http://tvthek.orf.at/programs/1360-Seitenblicke",True),
+            )
+          ),
+          TreeNode("1.8",u"Show","",False,
+            (
+              TreeNode("1.8.0",u"ARGE Talkshow","http://tvthek.orf.at/programs/1864371-ARGE-Talkshow",True),
+              TreeNode("1.8.1",u"Frisch gekocht","http://tvthek.orf.at/programs/1375-Frisch-gekocht",True),
+              TreeNode("1.8.2",u"Was gibt es Neues?","http://tvthek.orf.at/programs/1309553-Was-gibt-es-Neues-",True),
+              TreeNode("1.8.3",u"Willkommen Österreich","http://tvthek.orf.at/programs/1309549-Willkommen-Oesterreich",True),
+              TreeNode("1.8.4",u"Helden von Morgen - Die Show","http://tvthek.orf.at/programs/1679635-HvM---Die-Show",True),
+              TreeNode("1.8.5",u"Helden von Morgen - Die Entscheidung","http://tvthek.orf.at/programs/1710513-HvM---Die-Entscheidung",True),
+            )
+          ),
+          TreeNode("1.9",u"Volksgruppen","",False,
+            (
+              TreeNode("1.9.0",u"Heimat, fremde Heimat","http://tvthek.orf.at/programs/1357-Heimat--fremde-Heimat",True),
+              TreeNode("1.9.1",u"Dobar dan Hrvati","http://tvthek.orf.at/programs/85526-Dobar-dan-Hrvati",True),
+              TreeNode("1.9.2",u"Dober dan, Koroška","http://tvthek.orf.at/programs/85528-Dober-dan--Koroschka",True),
+              TreeNode("1.9.3",u"Adj' Isten magyarok","http://tvthek.orf.at/programs/85519-Adj--Isten-magyarok",True),
+              TreeNode("1.9.4",u"Servus Szia Zdravo Del tuha","http://tvthek.orf.at/programs/85512-Servus-Szia-Zdravo-Del-tuha",True),
+              TreeNode("1.9.5",u"České & Slovenské Ozveny","http://tvthek.orf.at/programs/181719-Tscheske---Slovenske-Ozveny",True),
+            )
+          ),          
         )
       )
     );
@@ -85,31 +130,66 @@ class ORFMediathek(Mediathek):
     self.regex_extractVideoLink = re.compile("/programs/.*.asx");
     self.regex_extractVideoObject = re.compile("<a href=\""+videoLinkPage+"\" title=\".*\">\\s*<span class=\"spcr\">\\s*<img src=\""+imageLink+"\" title=\".*\" alt=\".*\" />\\s*<span class=\".*\"></span>\\s*<strong>.*<span class=\"nowrap duration\">.*</span></strong>\\s*<span class=\"desc\">.*</span>\\s*</span>\\s*</a>");
     
+    self.regex_extractSearchObject = re.compile("<li class=\"clearfix\">\\s*<a href=\".*\" title=\".*\" class=\".*\"><img src=\".*\" alt=\".*\" /><span class=\"btn_play\">.*</span></a>\\s*<p>.*</p>\\s*<h4><a href=\".*\" title=\".*\">.*</a></h4>\\s*<p><a href=\".*\" title=\".*\"></a></p>\\s*</li>");
+    
+    self.regex_extractProgrammLink = re.compile("/programs/.*?\"");
+    self.regex_extractProgrammTitle = re.compile("title=\".*?\"");
+    self.regex_extractProgrammPicture = re.compile("/binaries/asset/segments/\\d*/image1");
+    
     self.replace_html = re.compile("<.*?>");
     
+    
+    self.searchLink = "http://tvthek.orf.at/search?q="
   @classmethod
   def name(self):
     return "ORF";
     
   def isSearchable(self):
-    return False;
+    return True;
+  
+  def createVideoLink(self,title,image,videoPageLink):
+    videoPage = self.loadPage(self.rootLink+videoPageLink);
+    
+    videoLink = self.regex_extractVideoLink.search(videoPage);
+    if(videoLink == None):
+      return;
+    
+    simpleLink = SimpleLink(self.rootLink+videoLink.group(), 0);
+    videoLink = {0:simpleLink};
+      
+    counter = 0
+    playlist = self.loadPage(simpleLink.basePath);
+    for line in playlist:
+      counter+=1;
+
+    if(counter == 1):
+      self.gui.buildVideoLink(DisplayObject(title,"",image,"",videoLink, True, time.gmtime()),self);
+    else:
+      self.gui.buildVideoLink(DisplayObject(title,"",image,"",videoLink, "PlayList", time.gmtime()),self);
+  
+  def searchVideo(self, searchText):
+    link = self.searchLink = "http://tvthek.orf.at/search?q="+searchText;
+    mainPage = self.loadPage(link);
+    
+    for searchObject in self.regex_extractSearchObject.findall(mainPage):
+      videoLink = self.regex_extractProgrammLink.search(searchObject).group().replace("\"","");
+      title = self.regex_extractProgrammTitle.search(searchObject).group().replace("title=\"","").replace("\"","");
+      pictureLink = self.regex_extractProgrammPicture.search(searchObject).group();
+      
+      self.createVideoLink(title,pictureLink,videoLink);
     
   def buildPageMenu(self, link):
     mainPage = self.loadPage(link);
     
     for linkObject in self.regex_extractVideoObject.findall(mainPage):
       
-      videoPageLink = self.regex_extractVideoPageLink.search(linkObject).group().replace("\"","");
-      videoPage = self.loadPage(self.rootLink+videoPageLink);
-      simpleLink = SimpleLink(self.rootLink+self.regex_extractVideoLink.search(videoPage).group(), 0);
-      videoLink = {0:simpleLink};
-      
+      videoLink = self.regex_extractVideoPageLink.search(linkObject).group().replace("\"","");
       
       image = self.regex_extractImageLink.search(linkObject).group();
       title = self.regex_extractTitle.search(linkObject).group().decode('UTF8');
       
       title = self.replace_html.sub("", title);
       title = title.replace(" <span","");
-      print image
-      #self.gui.buildVideoLink(DisplayObject(titles[0],titles[1],pictureLink,"",videoPageLink,False),self);
-      self.gui.buildVideoLink(DisplayObject(title,"",image,"",videoLink, True, time.gmtime()),self);
+      
+      self.createVideoLink(title,image,videoLink);
+
