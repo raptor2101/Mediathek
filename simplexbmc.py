f96d1a0d7e53ef23ea3c5d51e387b9d616b734b7
Raptor 2101 <raptor2101@gmx.de>
2012-11-15 09:09:58 +0100

    Fix: SyntaxError

--- simplexbmc.py
+++ simplexbmc.py
@@ -52,7 +52,7 @@ class SimpleXbmcGui(object):
     if displayObject.picture is not None:
       listItem=xbmcgui.ListItem(title, iconImage="DefaultFolder.png", thumbnailImage=displayObject.picture)
     else
-      else listItem=xbmcgui.ListItem(title, iconImage="DefaultFolder.png")
+      listItem=xbmcgui.ListItem(title, iconImage="DefaultFolder.png")
     
     if(displayObject.isPlayable):
       if(displayObject.isPlayable == "PlayList"):
