raptor2101/Mediathek

commit 8b2aedb618fb4cd0114dc9ae1d6326b170c332ca

CHG: rework to only load JSON Pages if needet

simplexbmc.py
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 import xbmc, xbmcgui, xbmcplugin,xbmcaddon, sys, urllib, os, time, re 
 from html import transformHtmlCodes
-from xml.dom import minidom
+import json
 
 regex_findLink = re.compile("mms://[^\"]*wmv");
 
@@ -27,12 +27,16 @@ settings = xbmcaddon.Addon(id='plugin.video.mediathek')
 translation = settings.getLocalizedString
 
 class SimpleXbmcGui(object):
-  def __init__(self):
+  def __init__(self,settings):
     self.settings = xbmcaddon.Addon(id='plugin.video.mediathek');
     self.quality = int(xbmcplugin.getSetting(int(sys.argv[1]), "quality" ));
     self.preferedStreamTyp = int(xbmcplugin.getSetting(int(sys.argv[1]), "preferedStreamType"));
     
     self.log("quality: %s"%(self.quality));
+    
+    self.plugin_profile_dir = xbmc.translatePath(settings.getAddonInfo("profile"))
+    if not os.path.exists(self.plugin_profile_dir):
+      os.mkdir(self.plugin_profile_dir);
     
   def log(self, msg):
     if type(msg) not in (str, unicode):
@@ -59,26 +63,17 @@ class SimpleXbmcGui(object):
         link = displayObject.link[0]
         
         url = "%s?type=%s&action=openPlayList&link=%s" % (sys.argv[0],mediathek.name(), urllib.quote_plus(link.basePath))
+        listItem.setProperty('IsPlayable', 'true');
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listItem,isFolder=False,totalItems = objectCount)
+      elif(displayObject.isPlayable == "JsonLink"):
+        link = displayObject.link
         
-        self.log(url);
-        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listItem,isFolder=True,totalItems = objectCount)
+        url = "%s?type=%s&action=openJsonLink&link=%s" % (sys.argv[0],mediathek.name(), urllib.quote_plus(link))
+        listItem.setProperty('IsPlayable', 'true');
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listItem,isFolder=False,totalItems = objectCount)
       else:
         self.log(displayObject.title);
-        if(self.quality in displayObject.link):
-          link = displayObject.link[self.quality];
-        else:
-          selectedKey = -1;
-          for key in displayObject.link.keys():
-            if(key < self.quality and key > selectedKey):
-              selectedKey = key;
-          if(selectedKey > -1):
-            link = displayObject.link[selectedKey];
-          else:
-            selectedKey = displayObject.link.keys()[0];
-            for key in displayObject.link.keys():
-              if(key < selectedKey):
-                selectedKey = key;
-            link = displayObject.link[selectedKey];
+        link = self.extractLink(links);
         
         if(type(link).__name__ == "ComplexLink"):
           self.log("PlayPath:"+ link.playPath);
@@ -109,6 +104,21 @@ class SimpleXbmcGui(object):
     listItem=xbmcgui.ListItem(title, iconImage="DefaultFolder.png")
            
     url = "%s?type=%s&action=openMenu&path=%s" % (sys.argv[0],mediathek.name(), menuObject.path)
+    xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listItem,isFolder=True,totalItems = objectCount)
+
+  def storeJsonFile(self,jsonObject):
+    storedJsonFile = os.path.join(self.plugin_profile_dir,"stored.json");
+    output = open(storedJsonFile, 'wb');
+    json.dump(jsonObject,output);
+  def loadJsonFile(self):
+    storedJsonFile = os.path.join(self.plugin_profile_dir,"stored.json");
+    input = open(storedJsonFile,"rb");
+    return json.load(input);
+
+  def buildJsonLink(self,mediathek,title,jsonPath,objectCount):
+    listItem=xbmcgui.ListItem(title, iconImage="DefaultFolder.png")
+    
+    url = "%s?type=%s&action=openJsonPath&path=%s" % (sys.argv[0],mediathek.name(), jsonPath)
     xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listItem,isFolder=True,totalItems = objectCount)
 
   def listAvaibleMediathekes(self, mediathekNames):
@@ -177,4 +187,25 @@ class SimpleXbmcGui(object):
     if(e == None):
       xbmcgui.Dialog().ok( title, msg, e )  
     else:
-      xbmcgui.Dialog().ok( title, msg)  
+      xbmcgui.Dialog().ok( title, msg)
+  def play(self,links):
+    link = self.extractLink(links);
+    listItem = xbmcgui.ListItem(path=link.basePath)
+    xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, listitem=listItem)
+    
+  def extractLink(self, links):
+    if(self.quality in links):
+      return links[self.quality];
+    else:
+      selectedKey = -1;
+      for key in links.keys():
+        if(key < self.quality and key > selectedKey):
+          selectedKey = key;
+      if(selectedKey > -1):
+        return links[selectedKey];
+      else:
+        selectedKey = links.keys()[0];
+        for key in links.keys():
+          if(key < selectedKey):
+            selectedKey = key;
+        return links[selectedKey];

default.py
@@ -16,12 +16,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 
-import urllib,xbmc,os
+import urllib,xbmc,os,xbmcaddon
 from simplexbmc import SimpleXbmcGui
 from mediathek.factory import MediathekFactory
 __plugin__ = "mediathek"
 
-gui = SimpleXbmcGui();
+settings = xbmcaddon.Addon(id='plugin.video.mediathek');
+
+gui = SimpleXbmcGui(settings);
 
 def get_params():
   paramDict = {}
@@ -41,6 +43,10 @@ def get_params():
 params = get_params();
 mediathekName = params.get("type", "")
 action=params.get("action", "")
+
+DIR_HOME = xbmc.translatePath(settings.getAddonInfo("profile"))
+if not os.path.exists(DIR_HOME):
+  os.mkdir(DIR_HOME);
 
 gui.log("Quality: %s"%gui.quality);
 gui.log("argv[0]: %s"%sys.argv[0]);
@@ -89,6 +95,12 @@ else:
       mediathek.searchVideo(searchText);
     else:
       gui.back();
+  elif(action == "openJsonPath"):
+    path = params.get("path", "0");
+    mediathek.buildJsonMenu(path,0)
+  elif(action == "openJsonLink"):
+    link = urllib.unquote_plus(params.get("link", ""));
+    mediathek.playVideoFromJsonLink(link);
   else:
     if(mediathek.isSearchable()):
       gui.addSearchButton(mediathek);

mediathek/__init__.py
@@ -120,3 +120,16 @@ class Mediathek(object):
         self.gui.buildMenuLink(treeNode,self,len(self.menuTree)) 
     else:
       self.buildPageMenu(self.menuTree[0].link, 0);
+      
+  def walkJson(self, path, jsonObject):
+    path = path.split('.');
+    i = 0
+    while(i < len(path)):
+      if(type(jsonObject) is list):
+        index = int(path.pop(0));
+      else:
+        index = path.pop(0);
+      jsonObject = jsonObject[index];
+      
+    return jsonObject;
+      

mediathek/zdf.py
@@ -70,21 +70,32 @@ class ZDFMediathek(Mediathek):
   def buildPageMenu(self, link, initCount):
     self.gui.log("buildPageMenu: "+link);
     jsonObject = json.loads(self.loadPage(link));
+    self.gui.storeJsonFile(jsonObject);
     
+    counter=0;
+    for clusterObject in jsonObject["cluster"]:
+      if clusterObject["type"]=="teaser":
+        path = "cluster.%d.teaser"%(counter)
+        self.gui.buildJsonLink(self,clusterObject["name"],path,initCount)
+      counter=counter+1;
+      
+  def buildJsonMenu(self, path, initCount):
+    jsonObject=self.gui.loadJsonFile();
+    jsonObject=self.walkJson(path,jsonObject);
+   
     categoriePages=[];
-    videoPages=[];
-    for categorie in jsonObject["cluster"]:
-      if "teaser" in categorie:
-        for videoObject in categorie["teaser"]:
-          self.gui.log(videoObject["type"])
-          if videoObject["type"] == "brand":
-            categoriePages.append(videoObject);
-          if videoObject["type"] == "video" and len(videoPages) < 50:
-            videoPages.append(videoObject);  
+    videoObjects=[];
+    counter=0;
     
-    counter=initCount+len(videoPages)+len(categoriePages);
+    for entry in jsonObject:
+      if entry["type"] == "brand":
+        categoriePages.append(entry);
+      if entry["type"] == "video" and len(videoObjects) < 50:
+        videoObjects.append(entry);  
+    
+    counter=initCount+len(videoObjects)+len(categoriePages);
     self.gui.log("CategoriePages: %d"%len(categoriePages));
-    self.gui.log("VideoPages: %d"%len(videoPages));  
+    self.gui.log("VideoPages: %d"%len(videoObjects));  
     for categoriePage in categoriePages:
       title=categoriePage["titel"];
       subTitle=categoriePage["beschreibung"];
@@ -97,48 +108,44 @@ class ZDFMediathek(Mediathek):
     
     
     
-    for videoPage in videoPages:
-      url = videoPage["url"];
-      jsonObject = json.loads(self.loadPage(url));
-      
-      documentObject = jsonObject["document"];
-      
-      title=documentObject["headline"];
-      subTitle=documentObject["titel"];
-      description=documentObject["beschreibung"];
-      imageLink="";
-      for width,imageObject in documentObject["teaserBild"].iteritems():
-        if int(width)<=840:
-          imageLink=imageObject["url"];
-      
-      links = {};
-      for formitaete in documentObject["formitaeten"]:
-        url = formitaete["url"];
-        quality = formitaete["quality"];
-        hd = formitaete["hd"];
-        self.gui.log("quality:%s hd:%s url:%s"%(quality,hd,url));
-        if hd == True:
-          links[4] = SimpleLink(url, -1); 
-        else:
-          if quality == "low":
-            links[0] = SimpleLink(url, -1); 
-          if quality == "med":
-            links[1] = SimpleLink(url, -1); 
-          if quality == "high":
-            links[2] = SimpleLink(url, -1); 
-          if quality == "veryhigh":
-            links[3] = SimpleLink(url, -1); 
-            
-            
-          
-        
-      
-      self.gui.buildVideoLink(DisplayObject(title,subTitle,imageLink,description,links,True),self,counter);
-      
-          
-          
-          
-          
+    for videoObject in videoObjects:
+      self.buildVideoLink(videoObject,counter);
       
       
-  
+  def buildVideoLink(self,videoObject,counter):
+    title=videoObject["headline"];
+    subTitle=videoObject["titel"];
+    description=videoObject["beschreibung"];
+    imageLink="";
+    for width,imageObject in videoObject["teaserBild"].iteritems():
+      if int(width)<=840:
+        imageLink=imageObject["url"];
+    link = videoObject["url"];
+    self.gui.buildVideoLink(DisplayObject(title,subTitle,imageLink,description,link,"JsonLink"),self,counter);
+    
+  def playVideoFromJsonLink(self,link):
+    jsonObject = json.loads(self.loadPage(link));
+    links={};
+    for formitaete in jsonObject["document"]["formitaeten"]:
+      url = formitaete["url"];
+      quality = formitaete["quality"];
+      hd = formitaete["hd"];
+      self.gui.log("quality:%s hd:%s url:%s"%(quality,hd,url));
+      if hd == True:
+        links[4] = SimpleLink(url, -1); 
+      else:
+        if quality == "low":
+          links[0] = SimpleLink(url, -1); 
+        if quality == "med":
+          links[1] = SimpleLink(url, -1); 
+        if quality == "high":
+          links[2] = SimpleLink(url, -1); 
+        if quality == "veryhigh":
+          links[3] = SimpleLink(url, -1);
+    self.gui.play(links);
+    
+      
+    
+
+    
+    

commit 37f6df1f74f859b6a265dba652d9b10c874c7a20

Fix crash when creating list of videos to play, introduced in 8b2aedb6.

simplexbmc.py
@@ -75,7 +75,7 @@ class SimpleXbmcGui(object):
         xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listItem,isFolder=False,totalItems = objectCount)
       else:
         self.log(displayObject.title);
-        link = self.extractLink(links);
+        link = self.extractLink(displayObject.link);
         
         if(type(link).__name__ == "ComplexLink"):
           self.log("PlayPath:"+ link.playPath);