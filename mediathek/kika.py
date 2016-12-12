442a60d9e4feaf26350affeb154e53e88174be81
raptor2101 <raptor2101@mykolab.com>
2016-12-12 20:47:59 +0100

    CHG: remove whitespace

--- mediathek/kika.py
+++ mediathek/kika.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import re, time;
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup;
 from mediathek import *
 
 class KIKA(Mediathek):
