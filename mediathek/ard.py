638394750846dcb0e65165c183f6434135491fa2
raptor2101 <raptor2101@mykolab.com>
2019-01-01 21:24:50 +0100

    CHG: getting ARD Mediathek working again ... close #108

--- mediathek/ard.py
+++ mediathek/ard.py
@@ -24,52 +24,19 @@ class ARDMediathek(Mediathek):
     self.gui = simpleXbmcGui;
     self.rootLink = "https://www.ardmediathek.de"
     self.menuTree = (
-                      TreeNode("0","Neuste Videos",self.rootLink+"/ard/",True),
-                      TreeNode("1","Sendungen von A-Z","",False,(
-                        TreeNode("1.0","0-9",self.rootLink+"/tv/sendungen-a-z?buchstabe=0-9",True),
-                        TreeNode("1.1","A",self.rootLink+"/tv/sendungen-a-z?buchstabe=A",True),
-                        TreeNode("1.2","B",self.rootLink+"/tv/sendungen-a-z?buchstabe=B",True),
-                        TreeNode("1.3","C",self.rootLink+"/tv/sendungen-a-z?buchstabe=C",True),
-                        TreeNode("1.4","D",self.rootLink+"/tv/sendungen-a-z?buchstabe=D",True),
-                        TreeNode("1.5","E",self.rootLink+"/tv/sendungen-a-z?buchstabe=E",True),
-                        TreeNode("1.6","F",self.rootLink+"/tv/sendungen-a-z?buchstabe=F",True),
-                        TreeNode("1.7","G",self.rootLink+"/tv/sendungen-a-z?buchstabe=G",True),
-                        TreeNode("1.8","H",self.rootLink+"/tv/sendungen-a-z?buchstabe=H",True),
-                        TreeNode("1.9","I",self.rootLink+"/tv/sendungen-a-z?buchstabe=I",True),
-                        TreeNode("1.10","J",self.rootLink+"/tv/sendungen-a-z?buchstabe=J",True),
-                        TreeNode("1.11","K",self.rootLink+"/tv/sendungen-a-z?buchstabe=K",True),
-                        TreeNode("1.12","L",self.rootLink+"/tv/sendungen-a-z?buchstabe=L",True),
-                        TreeNode("1.13","M",self.rootLink+"/tv/sendungen-a-z?buchstabe=M",True),
-                        TreeNode("1.14","N",self.rootLink+"/tv/sendungen-a-z?buchstabe=N",True),
-                        TreeNode("1.15","O",self.rootLink+"/tv/sendungen-a-z?buchstabe=O",True),
-                        TreeNode("1.16","P",self.rootLink+"/tv/sendungen-a-z?buchstabe=P",True),
-                        TreeNode("1.17","Q",self.rootLink+"/tv/sendungen-a-z?buchstabe=Q",True),
-                        TreeNode("1.18","R",self.rootLink+"/tv/sendungen-a-z?buchstabe=R",True),
-                        TreeNode("1.19","S",self.rootLink+"/tv/sendungen-a-z?buchstabe=S",True),
-                        TreeNode("1.20","T",self.rootLink+"/tv/sendungen-a-z?buchstabe=T",True),
-                        TreeNode("1.21","U",self.rootLink+"/tv/sendungen-a-z?buchstabe=U",True),
-                        TreeNode("1.22","V",self.rootLink+"/tv/sendungen-a-z?buchstabe=V",True),
-                        TreeNode("1.23","W",self.rootLink+"/tv/sendungen-a-z?buchstabe=W",True),
-                        TreeNode("1.24","X",self.rootLink+"/tv/sendungen-a-z?buchstabe=X",True),
-                        TreeNode("1.25","Y",self.rootLink+"/tv/sendungen-a-z?buchstabe=Y",True),
-                        TreeNode("1.26","Z",self.rootLink+"/tv/sendungen-a-z?buchstabe=Z",True),
-                        )),
-                      TreeNode("2","Ausgewählte Dokus".decode("utf-8"),self.rootLink+"/tv/Ausgew%C3%A4hlte-Dokus/mehr?documentId=33649086",True),
-                      TreeNode("3","Ausgewählte Filme".decode("utf-8"),self.rootLink+"/tv/Ausgew%C3%A4hlte-Filme/mehr?documentId=33649088",True),
-                      TreeNode("4","Alle Reportagen und Dokus",self.rootLink+"/tv/dokus",True),
-                      TreeNode("5","Alle Filme",self.rootLink+"/tv/filme",True),
-                      TreeNode("6","Nachrichten",self.rootLink+"/tv/nachrichten",True),
-                      TreeNode("7","Themen",self.rootLink+"/tv/Themen/mehr?documentId=21301810",True),
-                      TreeNode("8","Rubriken","",False,(
-                        TreeNode("8.0","Kinder",self.rootLink+"/tv/Kinder/Tipps?documentId=21282542",True),
-                        TreeNode("8.1","Unterhaltung & Comedy",self.rootLink+"/tv/unterhaltung",True),
-                        TreeNode("8.2","Kultur",self.rootLink+"/tv/kultur",True),
-                        TreeNode("8.3","Wissen",self.rootLink+"/tv/wissen",True),
-                        TreeNode("8.4","Politik",self.rootLink+"/tv/politik",True),
-                        TreeNode("8.5","Ratgeber",self.rootLink+"/tv/ratgeber",True),
-                        TreeNode("8.6","Sport",self.rootLink+"/tv/sport",True),
-                        TreeNode("8.7","Reise",self.rootLink+"/tv/reise",True),
-                        )),
+                      TreeNode("0" ,"ARD"          ,self.rootLink+"/ard/",True),
+                      TreeNode("1" ,"Das Erste"    ,self.rootLink+"/daserste/",True),
+                      TreeNode("2" ,"BR"           ,self.rootLink+"/br/",True),
+                      TreeNode("3" ,"HR"           ,self.rootLink+"/hr/",True),
+                      TreeNode("4" ,"MDR"          ,self.rootLink+"/mdr/",True),
+                      TreeNode("5" ,"NDR"          ,self.rootLink+"/ndr/",True),
+                      TreeNode("6" ,"Radio Bremen" ,self.rootLink+"/radiobremen/",True),
+                      TreeNode("7" ,"RBB"          ,self.rootLink+"/rbb/",True),
+                      TreeNode("8" ,"SR"           ,self.rootLink+"/sr/",True),
+                      TreeNode("9" ,"SWR"          ,self.rootLink+"/swr/",True),
+                      TreeNode("10","WDR"          ,self.rootLink+"/wdr/",True),
+                      TreeNode("11","ONE"          ,self.rootLink+"/one/",True),
+                      TreeNode("12","ARD-alpha"    ,self.rootLink+"/alpha/",True)
                       )
     self.configLink = self.rootLink+"/play/media/%s?devicetype=pc&feature=flash"
     self.regex_VideoPageLink = re.compile("<a href=\".*Video\?.*?documentId=(\d+).*?\" class=\"textLink\">\s+?<p class=\"dachzeile\">(.*?)<\/p>\s+?<h4 class=\"headline\">(.*?)<\/h4>\s+?<p class=\"subtitle\">(?:(\d+.\d+.\d+) \| )?(\d*) Min.")
@@ -85,7 +52,8 @@ class ARDMediathek(Mediathek):
 
 
     self.replace_html = re.compile("<.*?>");
-    
+    self.regex_DetermineClient = re.compile(self.rootLink+"/(.*)/");
+    self.categoryListingKey = "$ROOT_QUERY.widget({\"client\":\"%s\",\"pageNumber\":%s,\"pageSize\":%s,\"widgetId\":\"%s\"})"
     self.playerLink = self.rootLink+"/ard/player/%s"
     self.regex_ExtractJson = re.compile("__APOLLO_STATE__ = ({.*});");
     self.tumbnail_size = "600";
@@ -102,23 +70,61 @@ class ARDMediathek(Mediathek):
     jsonContent= pageContent.prettify(formatter=None);
     return json.loads(jsonContent);
 
-  def buildPageMenu(self, link, initCount, subLink = False):
-    self.gui.log("Build Page Menu: %s SubLink: %d"%(link,subLink));
+  def buildPageMenu(self, link, initCount):
+    self.gui.log("Build Page Menu: %s"%(link));
     jsonContent = self.extractJsonFromPage(link);
+    callHash = self.gui.storeJsonFile(jsonContent);
+    client = self.regex_DetermineClient.search(link).group(1);
+
     for key in jsonContent:
-      if(key.startswith("Teaser:")):
-        self.doSomeShit(jsonContent[key],jsonContent);
+      if(key.startswith("Widget:")):
+        self.GenerateCaterogyLink(jsonContent[key], callHash, jsonContent, client);
     return 0;
 
-  def doSomeShit(self, teaserContent, jsonContent):
+  def GenerateCaterogyLink(self, widgetContent, callHash, jsonContent,client):
+    widgetId = widgetContent["id"];
+    title = widgetContent["title"];
+    if(widgetContent["titleVisible"] == True):
+      self.gui.buildJsonLink(self, title, "%s.%s"%(client,widgetId), callHash,0);
+    else:
+      widgetContent = jsonContent[self.buildcategoryListingKey(client,widgetId,jsonContent)];
+      self.GenerateCaterogyLinks(widgetContent, jsonContent)
+
+  def buildcategoryListingKey(self,client,widgetId,jsonContent):
+    #ich werd zum elch... erst noch die "dynamische" Pagesize/Number nachschlagen ...
+    widgetContent = jsonContent["Widget:%s"%widgetId];
+    paginationId = widgetContent["pagination"]["id"];
+    paginationContent = jsonContent[paginationId];
+    pageSize = paginationContent["pageSize"];
+    pageNumber = paginationContent["pageNumber"];
+
+    return self.categoryListingKey%(client,pageNumber,pageSize,widgetId);
+
+  def buildJsonMenu(self, path, callhash, initCount):
+    jsonContent = self.gui.loadJsonFile(callhash);
+    path = path.split(".");
+    client = path[0];
+    widgetId = path[1];
+
+    widgetContent = jsonContent[self.buildcategoryListingKey(client,widgetId,jsonContent)];
+    self.GenerateCaterogyLinks(widgetContent, jsonContent)
+
+  def GenerateCaterogyLinks(self, widgetContent, jsonContent):
+    for teaser in widgetContent["teasers"]:
+      teaserId = teaser["id"];
+      self.GenerateVideoLink(jsonContent[teaserId],jsonContent);
+
+  def GenerateVideoLink(self, teaserContent, jsonContent):
     title = teaserContent["shortTitle"];
     subTitle = None;
     picture = self.getPictureLink(teaserContent["images"],jsonContent);
     videoLinks = self.getVideoLinks(teaserContent["links"],jsonContent);
-    date = None;
-    duration = None;
-    nodeCount = 0;
-    self.gui.buildVideoLink(DisplayObject(title, subTitle,picture,"",videoLinks,"JsonLink",date,duration),self,nodeCount);
+    if(teaserContent["broadcastedOn"] is not None):
+      date = time.strptime(teaserContent["broadcastedOn"],"%Y-%m-%dT%H:%M:%SZ");
+    else:
+      date = None;
+    duration = teaserContent["duration"];
+    self.gui.buildVideoLink(DisplayObject(title, subTitle, picture,"",videoLinks,"JsonLink",date,duration),self,0);
 
   def getVideoLinks(self, linkSource, jsonContent):
     #WTF geht es noch sinnloser?
